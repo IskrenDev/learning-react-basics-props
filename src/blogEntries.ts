d2d58eea4d978d365517c073336e7056593609a1
iskrenradev <iskrenr005@gmail.com>
2023-10-27 11:41:44 +0200

    Add BlogEntry type

---
+++ src/blogEntries.ts
@@ -0,0 +1,29 @@
+type BlogEntry = {
+    title: string,
+    imageURL: string,
+    imageAlt:string,
+    description: string
+}
+
+const blogEntries:BlogEntry[] = [
+    {
+        title: "Test1",
+        imageURL: "https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0%2C176%2C3008%2C1654&wid=4000&hei=2200&scl=0.752",
+        imageAlt: "abc",
+        description: "Testtesttest"
+    },
+    {
+        title: "Test2",
+        imageURL: "https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0%2C176%2C3008%2C1654&wid=4000&hei=2200&scl=0.752",
+        imageAlt: "abc",
+        description: "Testtesttest"
+    },
+    {
+        title: "Test3",
+        imageURL: "https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0%2C176%2C3008%2C1654&wid=4000&hei=2200&scl=0.752",
+        imageAlt: "abc",
+        description: "Testtesttest"
+    }
+]
+
+export default blogEntries
--- src/components/main/articles/ArticleList.tsx
+++ src/components/main/articles/ArticleList.tsx
@@ -1,14 +1,19 @@
 import Article from "./Article.tsx";
+import blogEntries from "../../../blogEntries.ts";
 
 function ArticleList() {
     return (
         <div className="articles-list">
-            <Article />
-            <Article />
-            <Article />
-            <Article />
-            <Article />
-            <Article />
+            {blogEntries.map((entry, index) => {
+                return <Article
+                    key = {index}
+                    title={entry.title}
+                    imageURL={entry.imageURL}
+                    imageAlt={entry.imageAlt}
+                    description={entry.description}
+                />
+            })}
+
         </div>
     )
 }
